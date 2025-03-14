0c4d4100201b090c93045c6e22e81d0552717e96
Luis Bertorelli <luisdgb2506@gmail.com>
2025-03-14 08:38:40 +0100

    dep

--- k3s/main/k3s/main.tf
+++ k3s/main/k3s/main.tf
@@ -325,7 +325,7 @@ resource "null_resource" "apply_argocd_repository" {
 
 resource "null_resource" "deploy_argocd_application" {
   depends_on = [
-    null_resource.install_argocd, apply_argocd_repository
+    null_resource.install_argocd, null_resource.apply_argocd_repository
   ]
 
   provisioner "remote-exec" {
